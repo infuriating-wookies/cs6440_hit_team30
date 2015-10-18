5d3e43b88a47ca1656c5bc0196003c1b247e196e
Nate Holland <nateh@spiceworks.com>
2015-10-18 15:06:01 -0500

    grab all the medications as well as the prescription

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -3,5 +3,10 @@ class MedicationsController < ApplicationController
 
   def index
     @medication_json = FhirConnection.get_patient_prescriptions(current_user.id)
+    medication_ids = []
+    @medication_json["entry"].each do |entry|
+      medication_ids << entry['resource']['medication']['reference']
+    end
+    @specific_medications = medication_ids.map { |e| FhirConnection.get_medication(e) }
   end
 end
