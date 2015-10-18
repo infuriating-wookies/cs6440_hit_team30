32773f23eab0d3defa33ae4da1d40546477c5c79
Nate Holland <nateh@spiceworks.com>
2015-10-18 14:31:25 -0500

    formatting the medication json a little prettier

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -2,6 +2,6 @@ class MedicationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render json: FhirConnection.get_patient_prescriptions(current_user.id)
+    @medication_json = FhirConnection.get_patient_prescriptions(current_user.id)
   end
 end
