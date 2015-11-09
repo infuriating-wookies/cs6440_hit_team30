132c84978a08fb75c11e9d0be80cf6608dc11aa4
Nate Holland <nateh@spiceworks.com>
2015-11-08 22:43:01 -0600

    adding super basic about us

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   resources :test, only: [:show]
   resources :medications, only: [:index]
   resources :basic_info, only: [:index]
+
+  resources :pages, only: [:index] do
+   collection do
+     get :about_us
+   end
+ end
   # You can have the root of your site routed with "root"
   root 'pages#index'
 
